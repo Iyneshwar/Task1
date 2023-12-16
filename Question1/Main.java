97e0445691338c7f8c209b3148b05d483fb39baf
Iyneshwar <153883175+Iyneshwar@users.noreply.github.com>
2023-12-16 20:15:22 +0530

    Add files via upload

---
+++ Question1/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //for loop
+        for (int i = 10; i <= 50; i++) {
+            System.out.println(i);
+
+        }
+
+        System.out.println(" ");
+
+        //while loop
+        int num = 10;
+        while (num <= 50) {
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
